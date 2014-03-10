03d9bdd34b292d6f6f53049271854b07146adf6c
Joshua Bell <inexorabletash@gmail.com>
2014-03-10 09:34:16 -0700

    Freeze template, comments

--- uate.js
+++ uate.js
@@ -1,6 +1,16 @@
 (function(global) {
 
-  function parse(s) {
+  function memoize(f) {
+    var cache = Object.create(null);
+    return function(arg) {
+      var key = '$' + arg;
+      if (key in cache) return cache[key];
+      return cache[key] = f(arg);
+    };
+  }
+
+  // Parser for template strings.
+  var parse = memoize(function(s) {
     var literalPortions = [], substitutions = [];
 
     var inLP = true, stack = [], literalPortion = '', tokens = '';
@@ -34,17 +44,7 @@
     literalPortions.push(literalPortion);
 
     return [literalPortions, substitutions];
-  }
-
-  function memoize(f) {
-    var cache = Object.create(null);
-    return function(arg) {
-      if (arg in cache) return cache[arg];
-      return cache[arg] = f(arg);
-    };
-  }
-
-  parse = memoize(parse);
+  });
 
   function functionName(f) {
     if (f === halfbaked) return 'halfbaked';
@@ -54,6 +54,8 @@
     return String(f).replace(/^function (.*)\([\s\S]*$/, '$1');
   }
 
+  // The magic happens here. This returns an eval()-ready string that
+  // performs the substitution using bindings from the local scope.
   function uate(a, b) {
     var tag = b === undefined ? undefined : a,
         template = b === undefined ? a : b;
@@ -72,6 +74,8 @@
       (substitutions.length ? ',' + substitutions.join(',') : '') + ')';
   }
 
+  // Helper function that approximates a 'raw' array of literal portions.
+  // A call to this is returned as part of the expression to eval().
   uate['uncook'] = function(cooked) {
     function escapeString(s) {
       return String(s).replace(/[^\x20-\x5b\x5d-\x7e]/g, function(c) {
@@ -89,10 +93,11 @@
         }
       });
     }
-    cooked['raw'] = cooked.map(escapeString);
-    return cooked;
+    return Object.freeze(Object.defineProperty(
+      cooked, 'raw', {value: cooked.map(escapeString)}));
   };
 
+  // The default template function.
   uate['default'] = function(callSite /*, ...substitutions*/) {
     var substitutions = [].slice.call(arguments, 1);
 
@@ -112,6 +117,7 @@
     }
   };
 
+  // Sample template function: like ES6's String.raw
   function halfbaked(callSite /*, ...substitutions*/) {
     var substitutions = [].slice.call(arguments, 1);
 
@@ -132,6 +138,7 @@
     }
   }
 
+  // Helper for simple template functions that just transform substitutions.
   function apply(f) {
     return function(callSite /*, ...substitutions*/) {
       var substitutions = [].slice.call(arguments, 1);
@@ -153,6 +160,7 @@
     };
   }
 
+  // Sample template function: HTML escaping of substitutions.
   var html = apply(function escapeHTML(text) {
     return text.replace(/[&<>"']/g, function(c) {
       switch (c) {
@@ -166,6 +174,7 @@
     });
   });
 
+  // Sample template function: URI encoding of substitutions.
   var uri = apply(encodeURIComponent);
 
   global['uate'] = uate;
